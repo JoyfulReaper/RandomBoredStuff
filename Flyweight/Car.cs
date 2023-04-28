0103772d74e03fcfebf1b9bdbe0c3bc5e468c2d6
Kyle Givler <me@kgivler.com>
2023-04-28 14:51:19 -0400

    Worked on Flyweight

---
+++ Flyweight/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace Flyweight;
+
+public class Car
+{
+    public string? Owner { get; set; }
+    public string? Number { get; set; }
+    public string Company { get; set; } = default!;
+    public string Model { get; set; } = default!;
+    public string Color { get; set; } = default!;
+}
