dbc973b34f54253f8d6ee664014e08d914e3aa1a
Kyle Givler <me@kgivler.com>
2023-05-09 16:09:57 -0400

    Units of Measure

---
+++ UnitsOfMeasure/Program.fs
@@ -0,0 +1,19 @@
+﻿module UnitsOfMeasure =
+
+    open Microsoft.FSharp.Data.UnitSystems.SI.UnitNames
+
+    let sampleValue1 = 1600.0<meter>
+
+    // Define a new unit type
+    [<Measure>]
+    type mile =
+        static member asMeter = 1609.34<meter/mile> // Conversion factor mile to meter.
+
+    /// Define a unitized constant
+    let sampleValue2 = 500.0<mile>
+
+    // Compute metric-system constant
+    let sampleValue3 = sampleValue2 * mile.asMeter
+
+    // Values using Units of Measure can be used just like the primitive numeric type for things like printing.
+    printfn $"After a %f{sampleValue1} race I would walk %f{sampleValue2} miles which would be %f{sampleValue3} meters"
