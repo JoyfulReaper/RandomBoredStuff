3f57d587fdc014aa2c80f387cfc5fdd0ef08315b
Kyle Givler <me@kgivler.com>
2023-05-10 09:22:00 -0400

    Generic Classes

--- ObjectProgramming/Program.fs
+++ ObjectProgramming/Program.fs
@@ -24,3 +24,28 @@
     let vector2 = vector1.Scale(10.0)
 
     printfn $"Length of vector1: %f{vector1.Length}\nLength of vector2: %f{vector2.Length}"
+
+
+    ///////////// Defining Generic Classes /////////////
+
+    type StateTracker<'T>(initialElement: 'T) =
+        /// Internal field to store the sate in a list
+        let mutable states = [ initialElement ]
+
+        /// Add a new element to the list of states
+        member this.UpdateState newState =
+            states <- newState :: states // use the '<-' operator to mutate the value.
+
+        /// Get the entire list of historical states.
+        member this.History = states
+
+        /// Get the latest state
+        member this.CurrentState = states.Head
+
+    /// An 'int' instance of the state tracker class. Note that the type parameter is inferred.
+    let tracker = StateTracker 10
+
+    tracker.UpdateState 17
+
+    tracker.History
+    |> List.iter (fun state -> printfn "State: %A" state)
