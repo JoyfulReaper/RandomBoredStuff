3603a797f53663f90cdaa986dcfe39ce3c367dd4
Kyle Givler <xvegan88x@gmail.com>
2023-05-25 21:33:19 -0400

    Guessing Game2...

---
+++ GuessGame2/Program.fs
@@ -0,0 +1,44 @@
+﻿open System
+
+type GuessResult =
+    | TooLow
+    | TooHigh
+    | Win
+
+let chooseSecretNumber() =
+    Random.Shared.Next(1, 101)
+
+let getGuess() =
+    printf "Guess a number between 1 and 100: "
+    Console.ReadLine() |> int
+
+let checkGuess (target: int) (guess: int) =
+    match compare guess target with
+    | 0 -> GuessResult.Win
+    | -1 -> GuessResult.TooLow
+    | 1 -> GuessResult.TooHigh
+    | _ -> failwith "Invalid guess"
+
+let printGuessResult guessResult =
+    match guessResult with
+    | GuessResult.Win -> printfn "You guessed correctly!"
+    | GuessResult.TooLow -> printfn "Too low!"
+    | GuessResult.TooHigh -> printfn "Too high!"
+    guessResult
+
+let guessGame() =
+    let target = chooseSecretNumber()
+    let rec gameLoop target turn =
+        let result = 
+            getGuess()
+            |> checkGuess target
+            |> printGuessResult
+
+        match result with
+        | GuessResult.Win -> printfn "You guessed in %d tries" turn
+        | _ when turn = 10 -> printfn "You lose! The number I was thinking of was %i" target
+        | _ -> gameLoop target (turn + 1)
+            
+    gameLoop target 1
+
+guessGame()
